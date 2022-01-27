c6415a505d65002c02aaac3660ed4e9112e6bc7c
Martin Pool <mbp@sourcefrog.net>
2022-01-27 12:00:33 -0800

    Factor out parameter scales

--- src/param.rs
+++ src/param.rs
@@ -20,11 +20,45 @@ const N_PARAM: usize = 8;
 // Scaling factors from the [0..1] range to the semantic range.
 const RELEASE_SCALE: f32 = 10.0;
 
+/// A scaling curve between the [0..1] range of a parameter, and the value
+/// used by the synth and shown in its text.
+enum Curve {
+    /// Simply map 0..1 to 0..1.
+    Identity,
+    /// Linear mapping to a different range.
+    Linear(f32, f32),
+    /// Exponentially scaled value between `2**a` and `2**b`.
+    Exp2(f32, f32),
+}
+
+impl Curve {
+    /// Scale forward from 0..1 to the output.
+    pub fn scale(&self, v: f32) -> f32 {
+        use Curve::*;
+        match self {
+            Identity => v,
+            Linear(a, b) => a + (b - a) * v,
+            Exp2(a, b) => (a + (b - a) * v).exp2(),
+        }
+    }
+
+    /// Reverse the transformation, to get back to a value in 0..1.
+    pub fn reverse(&self, y: f32) -> f32 {
+        use Curve::*;
+        match self {
+            Identity => y,
+            Linear(a, b) => (y - a) / (b - a),
+            Exp2(a, b) => (y.log2() - a) / (b - a),
+        }
+    }
+}
+
 /// A static definition of a single parameter.
 struct ParamDef {
     name: &'static str,
     label: &'static str,
     default: f32,
+    curve: Curve,
 }
 
 /// All the defined parameters, with indexes matching the constants above.
@@ -33,41 +67,49 @@ const PARAMS: [ParamDef; N_PARAM] = [
         name: "Attack",
         label: "s",
         default: 0.3,
+        curve: Curve::Identity,
     },
     ParamDef {
         name: "Decay",
         label: "s",
         default: 0.3,
+        curve: Curve::Identity,
     },
     ParamDef {
         name: "Sustain",
         label: "",
         default: 0.8,
+        curve: Curve::Identity,
     },
     ParamDef {
         name: "Release",
         label: "s",
         default: 0.1,
+        curve: Curve::Linear(0.0, 10.0),
     },
     ParamDef {
         name: "Osc1 Tune",
-        label: "",
+        label: "octave",
         default: 0.56,
+        curve: Curve::Linear(-1.0, 1.0),
     },
     ParamDef {
         name: "Osc1 Level",
         label: "",
         default: 0.5,
+        curve: Curve::Identity,
     },
     ParamDef {
         name: "Osc2 Tune",
-        label: "",
+        label: "octave",
         default: 0.43,
+        curve: Curve::Linear(-1.0, 1.0),
     },
     ParamDef {
         name: "Osc2 Level",
         label: "",
         default: 0.5,
+        curve: Curve::Identity,
     },
 ];
 
@@ -84,9 +126,13 @@ impl Params {
         N_PARAM
     }
 
+    fn scaled_value(&self, index: usize) -> f32 {
+        PARAMS[index].curve.scale(self.get_parameter(index as i32))
+    }
+
     /// Return the frequency multiplier for osc1.
     pub fn osc1_freq_mul(&self) -> f64 {
-        frequency_multiplier(self.copy_params()[OSC1_TUNE]) as f64
+        self.scaled_value(OSC1_TUNE).exp2() as f64
     }
 
     pub fn osc1_level(&self) -> f64 {
@@ -94,7 +140,7 @@ impl Params {
     }
 
     pub fn osc2_freq_mul(&self) -> f64 {
-        frequency_multiplier(self.copy_params()[OSC2_TUNE]) as f64
+        self.scaled_value(OSC2_TUNE).exp2() as f64
     }
 
     pub fn osc2_level(&self) -> f64 {
@@ -129,11 +175,6 @@ impl Default for Params {
     }
 }
 
-/// Scale a parameter in the range 0 to 1, to a frequency multiplier between 0.5 and 2.0.
-fn frequency_multiplier(a: f32) -> f32 {
-    (a * 2.0 - 1.0).exp2()
-}
-
 impl PluginParameters for Params {
     fn get_parameter(&self, index: i32) -> f32 {
         // This copies out all the parameters, which is OK and avoids locking.
@@ -154,10 +195,8 @@ impl PluginParameters for Params {
 
     fn get_parameter_text(&self, index: i32) -> String {
         let pval = self.get_parameter(index);
-        match index as usize {
-            RELEASE => format!("{:.3}", pval * RELEASE_SCALE),
-            _ => format!("{:.3}", pval),
-        }
+        let yval = PARAMS[index as usize].curve.scale(pval);
+        format!("{:.3}", yval)
     }
 
     fn get_parameter_label(&self, index: i32) -> String {
